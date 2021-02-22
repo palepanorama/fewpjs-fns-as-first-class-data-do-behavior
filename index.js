0e8537641281ab182f507d6d1091cbda27d42e67
palepanorama <matthew.keith.thomas@gmail.com>
2021-02-22 14:13:30 -0600

    commit tree 6742b8e1004a0fb85f61e609fd4166d88e2c360e

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let t = time.split(':')
+  let h = parseInt(t[0])
+  if (h < 12) {
+    return 'Good Morning'
+  } else if (h < 17) {
+    return 'Good Afternoon'
+  } else {
+    return 'Good Evening'
+  }
+  
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  let h1 = document.querySelector('#greeting')
+  h1.innerText = message 
+  
+}
